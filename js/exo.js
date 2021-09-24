888cb3b11eccb0c148317d77433f9dab4032ce78
Yannick Severo <severoyannick@gmail.com>
2021-09-24 09:29:19 +0200

    Étape 2 - Objet

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,11 @@
-console.log('test 1...2...1...2');
+// Informations sur Hercule
+const hercule = {
+  name: 'Hercule',
+  job: 'Demi-dieu',
+  age: 35,
+  department: 75,
+  arm: 60.5,
+  inRelationship: true,
+}
+// La méthode fillProfil est dans l'objet base qui est dans base.js
+base.fillProfil(hercule);
